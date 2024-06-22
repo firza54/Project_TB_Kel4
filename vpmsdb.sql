593dc48514f228238ba13682a76ce857259bb509
firza54 <clayforcefirza@gmail.com>
2024-06-22 15:22:02 +0700

    Add files via upload

---
+++ vpmsdb.sql
@@ -0,0 +1,185 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 22, 2024 at 09:58 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vpmsdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `ID` int(10) NOT NULL,
+  `AdminName` varchar(120) DEFAULT NULL,
+  `UserName` varchar(120) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `Password` varchar(120) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`ID`, `AdminName`, `UserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`) VALUES
+(1, 'Admin', 'admin', 7898799798, 'kelompok4@gmail.com', '7b78a34e865b851740ca3423f33684e4', '2019-07-05 05:38:23');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcategory`
+--
+
+CREATE TABLE `tblcategory` (
+  `ID` int(10) NOT NULL,
+  `VehicleCat` varchar(120) DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblcategory`
+--
+
+INSERT INTO `tblcategory` (`ID`, `VehicleCat`, `CreationDate`) VALUES
+(6, 'Beroda Empat', '2024-06-14 16:26:41'),
+(7, 'Beroda Tiga', '2024-06-14 16:26:57'),
+(8, 'Beroda Dua', '2024-06-14 16:27:07'),
+(9, 'Sepeda', '2024-06-14 16:27:14');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblregusers`
+--
+
+CREATE TABLE `tblregusers` (
+  `ID` int(5) NOT NULL,
+  `FirstName` varchar(250) DEFAULT NULL,
+  `LastName` varchar(250) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(250) DEFAULT NULL,
+  `Password` varchar(250) DEFAULT NULL,
+  `RegDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblregusers`
+--
+
+INSERT INTO `tblregusers` (`ID`, `FirstName`, `LastName`, `MobileNumber`, `Email`, `Password`, `RegDate`) VALUES
+(3, 'Firza', 'Much', 8593956542, '2206010@itg.ac.id', '3fb2b4407ed6c7d13029b98e5ad3fdef', '2024-06-15 04:51:28');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblvehicle`
+--
+
+CREATE TABLE `tblvehicle` (
+  `ID` int(10) NOT NULL,
+  `ParkingNumber` varchar(120) DEFAULT NULL,
+  `VehicleCategory` varchar(120) NOT NULL,
+  `VehicleCompanyname` varchar(120) DEFAULT NULL,
+  `RegistrationNumber` varchar(120) DEFAULT NULL,
+  `OwnerName` varchar(120) DEFAULT NULL,
+  `OwnerContactNumber` bigint(10) DEFAULT NULL,
+  `InTime` timestamp NULL DEFAULT current_timestamp(),
+  `OutTime` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `ParkingCharge` varchar(120) NOT NULL,
+  `Remark` mediumtext NOT NULL,
+  `Status` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblvehicle`
+--
+
+INSERT INTO `tblvehicle` (`ID`, `ParkingNumber`, `VehicleCategory`, `VehicleCompanyname`, `RegistrationNumber`, `OwnerName`, `OwnerContactNumber`, `InTime`, `OutTime`, `ParkingCharge`, `Remark`, `Status`) VALUES
+(10, '216407668', 'Beroda Empat', 'Lexus', 'F5425', 'Firza', 8593956542, '2024-06-15 02:36:57', '2024-06-15 02:37:20', '5000', 'Aman', 'Out'),
+(11, '557284404', 'Beroda Empat', 'Avanza', 'Z5414', 'Hilmi', 85798955, '2024-06-15 02:39:07', '2024-06-15 09:24:34', '5000', 'Kendaraan Sesuai', 'Out'),
+(12, '777372010', '0', 'Toyota', 'Z 5656 F', 'Naufal', 85666458, '2024-06-15 07:01:30', NULL, '', '', ''),
+(13, '511647101', 'Beroda Empat', 'Xiaomi', 'Z 5456 B', 'Asep', 856696, '2024-06-15 09:23:19', NULL, '', '', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblcategory`
+--
+ALTER TABLE `tblcategory`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `VehicleCat` (`VehicleCat`);
+
+--
+-- Indexes for table `tblregusers`
+--
+ALTER TABLE `tblregusers`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `MobileNumber` (`MobileNumber`);
+
+--
+-- Indexes for table `tblvehicle`
+--
+ALTER TABLE `tblvehicle`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblcategory`
+--
+ALTER TABLE `tblcategory`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `tblregusers`
+--
+ALTER TABLE `tblregusers`
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tblvehicle`
+--
+ALTER TABLE `tblvehicle`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
