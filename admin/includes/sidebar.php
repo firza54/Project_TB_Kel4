aa806b57235a165a65ba067e83957b2fe241f456
firza54 <clayforcefirza@gmail.com>
2024-06-22 15:20:16 +0700

    Add files via upload

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,46 @@
+ <aside id="left-panel" class="left-panel">
+        <nav class="navbar navbar-expand-sm navbar-default">
+            <div id="main-menu" class="main-menu collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active">
+                        <a href="dashboard.php"><i class="menu-icon fa fa-laptop"></i>Dashboard </a>
+                    </li>
+                   
+                   
+                    <li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-table"></i>Tipe Kendaraan</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><i class="fa fa-table"></i><a href="add-category.php">Tambah Kategori</a></li>
+                            <li><i class="fa fa-table"></i><a href="manage-category.php">Kelola Kategori</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="add-vehicle.php"> <i class="menu-icon ti-email"></i>Tambah Kendaraan </a>
+                    </li>
+                    <li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-th"></i>Kelola Kendaraan</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><i class="menu-icon fa fa-th"></i><a href="manage-incomingvehicle.php">Kelola Kendaraan Masuk</a></li>
+                            <li><i class="menu-icon fa fa-th"></i><a href="manage-outgoingvehicle.php">Kelola Kendaraan Keluar</a>
+                           
+                        </li>
+
+                        </ul>
+                    </li>
+                    <li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-th"></i>Laporan</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><i class="menu-icon fa fa-th"></i><a href="bwdates-report-ds.php">Laporan Antara</a></li>
+                           
+                        </ul>
+                    </li>
+<li>
+                        <a href="search-vehicle.php"> <i class="menu-icon fa fa-search"></i>Cari Kendaraan </a>
+                    </li>
+                    <li>
+                        <a href="reg-users.php"> <i class="menu-icon ti-user"></i>Pengguna Terdaftar </a>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </nav>
+    </aside>
